12cee549e003136310c4f1a6acb2bce3deed1287
Daanyaal Sobani <daanyaal.sobani@gmail.com>
2026-04-23 11:04:25 -0400

    fixed to u64 from u32

--- problems/problem-zero/src/main.rs
+++ problems/problem-zero/src/main.rs
@@ -1,19 +1,19 @@
 use std::env::args;
 
-fn good_solution(n: u32) -> u32 {
+fn good_solution(n: u64) -> u64 {
     (0..=n).filter(|x| x%2!=0).map(|x| x*x).sum()
 }
-fn better_solution(n: u32) -> u32 {
+fn better_solution(n: u64) -> u64 {
     let n = (n-1)/2;
     (0..=n).map(|x| (2*x+1)*(2*x+1)).sum()
 }
-fn best_solution(n:u32)-> u32 {
+fn best_solution(n: u64) -> u64 {
     let n = (n+1)/2;
-    (n)*(4*n*n-1)/3
+    n*(4*n*n-1)/3
 }
 
 fn main() {
-    let big_n : u32 = args().nth(1).expect("Please provide an argument").parse().expect("Please provide a valid number");
+    let big_n: u64 = args().nth(1).expect("Please provide an argument").parse().expect("Please provide a valid number");
     let result = good_solution(big_n);
     println!("N = {}, Result = {}", big_n, result);
     
